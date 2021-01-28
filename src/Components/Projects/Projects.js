fba18ffb81f2808437995f08d08d8dca002a5be7
Stephen McAleese <stephenmcaleese11@gmail.com>
2021-01-28 21:22:31 +0000

    added Weatherly and MyWhist to projects

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -3,6 +3,8 @@ import Navigation from '../../Containers/Navigation/Navigation'
 import covidForcasterImage from '../../images/covid-forecaster.png'
 import jsClockImage from '../../images/js-clock.png'
 import chitchatImage from '../../images/chitchat-updated.png'
+import whistImage from '../../images/whist.png'
+import weatherlyImage from '../../images/weatherly.png'
 import './Projects.scss'
 
 class Projects extends Component {
@@ -16,7 +18,61 @@ class Projects extends Component {
                         <p>some of the things I've recently been working on</p>
                     </div>
                     <div className="projects-list">
-                    <div className="project">
+                        <div className="project">
+                            <div className="project-description-box center">
+                                <div className="project-description">
+                                    <h2>Weatherly</h2>
+                                    <p>
+                                        A web app which shows weather information about any location in the world.
+                                        It sources weather data from the Open Weather API.
+                                    </p>
+                                    <p>Technologies used: Javascript, HTML, CSS.</p>
+                                    <p>Date finished: January 2021</p>
+                                </div>
+                                <div className="buttons-container">
+                                    <a href="https://weatherly-online.herokuapp.com">
+                                        <button type="button" className="btn btn-primary">open live app</button>
+                                    </a>
+                                    <a href="https://github.com/smcaleese/weatherly">
+                                        <button type="button" className="btn btn-danger">open source</button>
+                                    </a>
+                                </div>
+                            </div>
+                            <figure className="project-image-container center">
+                                <a href={weatherlyImage}>
+                                    <img src={weatherlyImage} alt="" />
+                                </a>
+                            </figure>
+                        </div>
+
+                        <div className="project">
+                            <div className="project-description-box center">
+                                <div className="project-description">
+                                    <h2>Whist Card Game</h2>
+                                    <p>
+                                        A single-player implementation of the card game Whist in the browser. There is
+                                        one AI player on your team and two AI players on the opposing team.
+                                    </p>
+                                    <p>Technologies used: ReactJS, NodeJS, SocketIO</p>
+                                    <p>Date finished: December 2020</p>
+                                </div>
+                                <div className="buttons-container">
+                                    <a href="https://mywhist.herokuapp.com/">
+                                        <button type="button" className="btn btn-primary">open live app</button>
+                                    </a>
+                                    <a href="https://github.com/smcaleese/whist-singleplayer">
+                                        <button type="button" className="btn btn-danger">open source</button>
+                                    </a>
+                                </div>
+                            </div>
+                            <figure className="project-image-container center">
+                                <a href={whistImage}>
+                                        <img src={whistImage} alt="" />
+                                </a>
+                            </figure>
+                        </div>
+
+                        <div className="project">
                             <div className="project-description-box center">
                                 <div className="project-description">
                                     <h2>ChitChat</h2>
@@ -35,7 +91,7 @@ class Projects extends Component {
                                 </div>
                             </div>
                             <figure className="project-image-container center">
-                                <a href={chitchatImage}>
+                                <a href={weatherlyImage}>
                                         <img src={chitchatImage} alt="" />
                                 </a>
                             </figure>
