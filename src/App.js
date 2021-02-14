4294a1ce56649028b96a74a716382f1fc5a29754
Stephen McAleese <stephenmcaleese11@gmail.com>
2021-02-14 19:18:25 +0000

    added process.env.PUBLIC_URL to React routes

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import About from './Components/About/About'
 import Projects from './Components/Projects/Projects'
 import Resume from './Components/Resume/Resume'
 
+const root = process.env.PUBLIC_URL
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path="/" component={About} />
-          <Route path="/projects" component={Projects} />
-          <Route path="/resume" component={Resume} />
+          <Route exact path={root + "/"} component={About} />
+          <Route path={root + "/projects"} component={Projects} />
+          <Route path={root + "/resume"} component={Resume} />
         </Switch>
       </Router>
     </div>
