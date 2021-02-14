4bdad134a693da2c693339423fa08816d7598321
Stephen McAleese <stephenmcaleese11@gmail.com>
2021-02-14 19:41:19 +0000

    added HashRouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 import About from './Components/About/About'
 import Projects from './Components/Projects/Projects'
 import Resume from './Components/Resume/Resume'
 
-const root = process.env.PUBLIC_URL
-
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Switch>
-          <Route exact path={root + "/"} component={About} />
-          <Route path={root + "/projects"} component={Projects} />
-          <Route path={root + "/resume"} component={Resume} />
+          <Route exact path="/" component={About} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/resume" component={Resume} />
         </Switch>
       </Router>
     </div>
--- src/Containers/Navigation/Navigation.js
+++ src/Containers/Navigation/Navigation.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import { Link } from 'react-router-dom'
 
 class Navigation extends Component {
     render() {
         return(
             <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="/">Stephen McAleese</Navbar.Brand>
+                <Navbar.Brand as={Link} to="/">Stephen McAleese</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                   <Nav className="mr-auto">
-                    <Nav.Link href="/">About</Nav.Link>
-                    <Nav.Link href="/projects">Projects</Nav.Link>
-                    <Nav.Link href="/resume">Resume</Nav.Link>
+                    <Nav.Link as={Link} to="/">About</Nav.Link>
+                    <Nav.Link as={Link} to="/projects">Projects</Nav.Link>
+                    <Nav.Link as={Link} to="/resume">Resume</Nav.Link>
                   </Nav>
                 </Navbar.Collapse>
             </Navbar>
