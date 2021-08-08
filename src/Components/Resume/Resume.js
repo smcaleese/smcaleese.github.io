3aa5bd5b2ae4404748db46e0cb316660d63c5560
Stephen McAleese <stephenmcaleese11@gmail.com>
2021-08-08 19:38:43 +0100

    Added new resume and replaced Resume class component with function component

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -1,23 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Navigation from '../../Containers/Navigation/Navigation'
-import resume from './resume-30-10-20.pdf'
+import resume from './resume-2021.pdf'
 import './Resume.scss'
 
-class Resume extends Component {
-    render() {
-        return(
-            <div>
-                <Navigation />
-                <div className="resume-header">
-                    <h1>Resume</h1>
-                    <a href={resume} download>
-                        <button type="button" className="btn btn-primary resume-button">Click here to download my resume</button>
-                    </a>
-                </div>
-
-            </div>
-        )
-    }
-}
+const Resume = () => (
+    <>
+        <Navigation />
+        <div className="resume-header">
+            <h1>Resume</h1>
+            <a href={resume} download>
+                <button type="button" className="btn btn-primary resume-button">
+                    Click here to download my resume
+                </button>
+            </a>
+        </div>
+    </>
+)
 
 export default Resume
