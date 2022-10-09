983b9d03de750724f6285bfde167cf644fba3393
Stephen McAleese <stephenmcaleese11@gmail.com>
2022-10-09 22:30:10 +0100

    Added recommend column to books page

--- books.py
+++ books.py
@@ -1,14 +1,13 @@
-import csv
+import pandas as pd
 
 def main():
-    with open('bookshelf.csv', newline='') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        rows = [row for row in csv_reader][1:]
-
+    book_data = pd.read_csv('bookshelf.csv')
     with open('output.txt', 'w') as f:
-        for row in rows:
-            title, author = row
-            html_string = f'<tr><td>{title}</td><td>{author}</td></tr>\n'
+        for index, row in book_data.iterrows():
+            title, author, recommend = row['title'], row['author'], row['recommend']
+            star_str = '&#11088;'
+            recommend_str = star_str if recommend == 'yes' else ''
+            html_string = f'<tr><td>{title}</td><td>{author}</td><td class="has-text-centered">{recommend_str}</td></tr>\n'
             f.write(html_string)
 
 if __name__ == '__main__':
